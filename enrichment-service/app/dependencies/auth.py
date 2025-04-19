53cb2b6de891c8653eb07aafca461dfac63c4787
Agnostk <agn0stkp@gmail.com>
2025-04-19 21:11:35 +0900

    refactor(enrichment): use x-api-key as security header param

--- enrichment-service/app/dependencies/auth.py
+++ enrichment-service/app/dependencies/auth.py
@@ -1,11 +1,14 @@
-from fastapi import Header, HTTPException, status, Request
+from fastapi import HTTPException, status, Request, Security
+from fastapi.security import APIKeyHeader
 
 from app.config import API_KEY
 
+header_scheme = APIKeyHeader(name="x-api-key", auto_error=False)
 
-def verify_api_key(x_api_key: str = Header(...), request: Request = None):
-    # Allow public access to health/docs
-    if request.url.path in ["/health", "/docs", "/openapi.json"]:
+
+def verify_api_key(x_api_key: str = Security(header_scheme), request: Request = None, ):
+    # Allow public access to docs
+    if request.url.path in ["/docs", "/openapi.json"]:
         return
     if x_api_key != API_KEY:
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid API Key")
